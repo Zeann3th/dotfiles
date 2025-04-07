71970bddd9eff99a2d7d4e7298d819ed80e745a7
zeann3th <longhienly112@gmail.com>
2025-04-07 11:21:01 +0700

    feat: ps1 for alias

---
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,7 @@
+Set-Alias vi nvim
+
+Set-Alias make mingw32-make
+
+Invoke-Expression (&starship init powershell)
+
+#if ( Test-Path '~/.inshellisense/pwsh/init.ps1' -PathType Leaf ) { . ~/.inshellisense/pwsh/init.ps1 } 
