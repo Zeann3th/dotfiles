d4f93705ce79d836c79393ba5cc836f36a61afbd
zeann3th <longhienly112@gmail.com>
2025-04-07 11:54:13 +0700

    feat: some config for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,21 +1,15 @@
 -- Credits to: vimichael @ https://github.com/vimichael/make-windows-pretty/blob/main/.wezterm.lua
 
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 local act = wezterm.action
--- local mux = wezterm.mux
--- This will hold the configuration.
 local config = wezterm.config_builder()
--- local gpus = wezterm.gui.enumerate_gpus()
--- config.webgpu_preferred_adapter = gpus[1]
--- config.front_end = "WebGpu"
 
 config.front_end = "OpenGL"
 config.max_fps = 144
 config.default_cursor_style = "BlinkingBlock"
 config.animation_fps = 1
 config.cursor_blink_rate = 500
-config.term = "xterm-256color" -- Set the terminal type
+config.term = "xterm-256color"
 
 config.font = wezterm.font("JetBrainsMonoNL Nerd Font Mono")
 config.cell_width = 0.9
@@ -33,37 +27,17 @@ config.window_padding = {
 -- tabs
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
--- config.tab_bar_at_bottom = true
-
--- config.inactive_pane_hsb = {
--- 	saturation = 0.0,
--- 	brightness = 1.0,
--- }
-
--- This is where you actually apply your config choices
---
-
--- color scheme toggling
-wezterm.on("toggle-colorscheme", function(window, pane)
-	local overrides = window:get_config_overrides() or {}
-	if overrides.color_scheme == "Zenburn" then
-		overrides.color_scheme = "Cloud (terminal.sexy)"
-	else
-		overrides.color_scheme = "Zenburn"
-	end
-	window:set_config_overrides(overrides)
-end)
 
 -- keymaps
 config.keys = {
 	{
-		key = "E",
-		mods = "CTRL|SHIFT|ALT",
-		action = wezterm.action.EmitEvent("toggle-colorscheme"),
+		key = "w",
+		mods = "CTRL",
+		action = wezterm.action.CloseCurrentPane({ confirm = false }),
 	},
 	{
-		key = "|",
-		mods = "CTRL|SHIFT|ALT",
+		key = "\\",
+		mods = "ALT",
 		action = wezterm.action.SplitPane({
 			direction = "Right",
 			size = { Percent = 50 },
@@ -71,7 +45,7 @@ config.keys = {
 	},
 	{
 		key = "-",
-		mods = "CTRL|SHIFT|ALT",
+		mods = "ALT",
 		action = wezterm.action.SplitPane({
 			direction = "Down",
 			size = { Percent = 50 },
@@ -115,26 +89,13 @@ config.keys = {
 	},
 }
 
--- For example, changing the color scheme:
-config.color_scheme = "Cloud (terminal.sexy)"
 config.colors = {
-	-- background = '#3b224c',
-	-- background = "#181616", -- vague.nvim bg
-	-- background = "#080808", -- almost black
 	background = "#0c0b0f", -- dark purple
-	-- background = "#020202", -- dark purple
-	-- background = "#17151c", -- brighter purple
-	-- background = "#16141a",
-	-- background = "#0e0e12", -- bright washed lavendar
-	-- background = 'rgba(59, 34, 76, 100%)',
 	cursor_border = "#bea3c7",
-	-- cursor_fg = "#281733",
 	cursor_bg = "#bea3c7",
-	-- selection_fg = '#281733',
 
 	tab_bar = {
 		background = "#0c0b0f",
-		-- background = "rgba(0, 0, 0, 0%)",
 		active_tab = {
 			bg_color = "#0c0b0f",
 			fg_color = "#bea3c7",
@@ -153,7 +114,6 @@ config.colors = {
 		},
 
 		new_tab = {
-			-- bg_color = "rgba(59, 34, 76, 50%)",
 			bg_color = "#0c0b0f",
 			fg_color = "white",
 		},
@@ -161,30 +121,12 @@ config.colors = {
 }
 
 config.window_frame = {
-	font = wezterm.font({ family = "Iosevka Custom", weight = "Regular" }),
+	font = wezterm.font({ family = "JetBrainsMonoNL Nerd Font Mono", weight = "Regular" }),
 	active_titlebar_bg = "#0c0b0f",
-	-- active_titlebar_bg = "#181616",
 }
 
--- config.window_decorations = "INTEGRATED_BUTTONS | RESIZE"
 config.window_decorations = "NONE | RESIZE"
 config.default_prog = { "pwsh.exe", "-NoLogo" }
 config.initial_cols = 80
--- config.window_background_image = "C:/dev/misc/berk.png"
--- config.window_background_image_hsb = {
--- 	brightness = 0.1,
--- }
 
--- wezterm.on("gui-startup", function(cmd)
--- 	local args = {}
--- 	if cmd then
--- 		args = cmd.args
--- 	end
---
--- 	local tab, pane, window = mux.spawn_window(cmd or {})
--- 	-- window:gui_window():maximize()
--- 	-- window:gui_window():set_position(0, 0)
--- end)
-
--- and finally, return the configuration to wezterm
 return config
